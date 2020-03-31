67a0341a558fabf5ba81bd7f5b3d19bfca131043
Erin Sheldon <erin.sheldon@gmail.com>
2020-03-31 13:06:08 -0400

    zero out bad psf pixels
    The idea here is to maintain a large psf image for the deconvolutions
    downstream.  The zeroing seems to improve the problem of errors
    when deconvolving bright stars, at least visually.
    
    This strategy may work fine for lots of epochs, I'm less sure about few
    epochs.  I plan to run some r+i+z tests.
    
    This accompanies a change in mdet-lsst-sim where the psf dim from the
    original sim psf is propagated rather than a trimmed version

--- descwl_coadd/coadd.py
+++ descwl_coadd/coadd.py
@@ -326,6 +326,8 @@ class CoaddObs(ngmix.Observation):
         )
         coadd_psf_exp = self._make_coadd(**psf_data)
         pimage = coadd_psf_exp.image.array
+        wbad = np.where(~np.isfinite(pimage))
+        pimage[wbad] = 0.0
         if np.any(~np.isfinite(pimage)):
             raise ValueError('some bad pixels in coadd psf')
 
