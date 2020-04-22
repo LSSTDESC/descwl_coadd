b65bf4a685832e7c21394b1235829c16486bfafd
Erin Sheldon <erin.sheldon@gmail.com>
2020-04-22 18:54:17 -0400

    flag BRIGHT as SAT to prevent detectioins in those regions

--- descwl_coadd/coadd.py
+++ descwl_coadd/coadd.py
@@ -73,10 +73,6 @@ class MultiBandCoadds(object):
         )
         self.replace_bright = replace_bright
         self.interp_bright = interp_bright
-        if self.interp_bright:
-            self.flags2interp = FLAGS2INTERP | BRIGHT
-        else:
-            self.flags2interp = FLAGS2INTERP
 
         self._show = show
         self.log = lsst.log.getLogger("MultiBandCoadds")
@@ -163,13 +159,15 @@ class MultiBandCoadds(object):
 
                 if not self.use_stack_interp:
                     zero_bits(image=image, noise=noise, mask=bmask, flags=EDGE)
+                    if self.interp_bright:
+                        flag_bright_as_interp(mask=bmask)
 
                     image, noise = interpolate_image_and_noise(
                         image=image,
                         noise=noise,
                         weight=weight,
                         bmask=bmask,
-                        bad_flags=self.flags2interp,
+                        bad_flags=FLAGS2INTERP,
                     )
                     if self._show:
                         vis.show_images([
@@ -755,10 +753,9 @@ def replace_bright_with_noise(*, rng, image, noise_image, weight, mask):
         wtravel[w] = medweight
 
 
-'''
-def flag_bright_star_as_interp(*, mask):
+def flag_bright_as_interp(*, mask):
     """
-    flag BRIGHT also as SAT so it will be interpolated
+    flag BRIGHT also as SAT so no detections will occur there
 
     we currently pull the bitmask value from the descwl_shear_sims
     package
@@ -768,4 +765,3 @@ def flag_bright_star_as_interp(*, mask):
     if w[0].size > 0:
         SAT = afw_image.Mask.getPlaneBitMask('SAT')
         mask[w] |= SAT
-'''
