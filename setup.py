9a92bba5dedbe9ea796a7553fd00c35b328583c4
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-12-01 06:35:25 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="descwl_coadd",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=['numpy'],
     include_package_data=True,
     author='Erin S. Sheldon',
     author_email='erin.sheldon@gmail.com',
