2690321bd276103401a1a32bd10c2fb3588f8725
Erin Sheldon <erin.sheldon@gmail.com>
2021-06-01 15:56:34 -0400

    update for new code

--- descwl_coadd/tests/test_coadd_sim.py
+++ descwl_coadd/tests/test_coadd_sim.py
@@ -2,19 +2,14 @@ import os
 import pytest
 import numpy as np
 
-from descwl_shear_sims.sim import (
-    make_dmsim,
-    make_galaxy_catalog,
-    StarCatalog,
-    make_psf,
-    make_ps_psf,
-    get_se_dim,
-)
-
-from ..coadd import MultiBandCoaddsDM
+from descwl_shear_sims.sim import make_sim, get_se_dim
+from descwl_shear_sims.galaxies import make_galaxy_catalog
+from descwl_shear_sims.stars import StarCatalog
+from descwl_shear_sims.psfs import make_fixed_psf, make_ps_psf
+from ..online_coadds import make_online_coadd_obs
 
 
-def _make_sim(rng, psf_type, epochs_per_band=3, stars=False):
+def _make_sim(rng, psf_type, epochs_per_band=3, stars=False, dither=True, rotate=True):
     seed = 431
     rng = np.random.RandomState(seed)
     buff = 5
@@ -32,7 +27,7 @@ def _make_sim(rng, psf_type, epochs_per_band=3, stars=False):
         se_dim = get_se_dim(coadd_dim=coadd_dim)
         psf = make_ps_psf(rng=rng, dim=se_dim)
     else:
-        psf = make_psf(psf_type=psf_type)
+        psf = make_fixed_psf(psf_type=psf_type)
 
     if stars:
         star_catalog = StarCatalog(
@@ -44,7 +39,7 @@ def _make_sim(rng, psf_type, epochs_per_band=3, stars=False):
     else:
         star_catalog = None
 
-    return make_dmsim(
+    return make_sim(
         rng=rng,
         galaxy_catalog=galaxy_catalog,
         star_catalog=star_catalog,
@@ -54,8 +49,8 @@ def _make_sim(rng, psf_type, epochs_per_band=3, stars=False):
         g2=0.00,
         bands=['i', 'z'],
         psf=psf,
-        dither=True,
-        rotate=True,
+        dither=dither,
+        rotate=dither,
     )
 
 
@@ -70,38 +65,19 @@ def test_coadd_sim_psgauss_smoke():
 
     psf_dims = data['psf_dims']
 
-    # coadding individual bands as well as over bands
-    coadds = MultiBandCoaddsDM(
-        data=data['band_data'],
-        coadd_wcs=data['coadd_wcs'],
-        coadd_bbox=data['coadd_bbox'],
-        psf_dims=psf_dims,
-    )
+    for band, exps in data['band_data'].items():
+        coadd = make_online_coadd_obs(
+            exps=exps,
+            coadd_wcs=data['coadd_wcs'],
+            coadd_bbox=data['coadd_bbox'],
+            psf_dims=psf_dims,
+            rng=rng,
+            remove_poisson=False,
+        )
 
-    coadd = coadds.get_coadd()
-    assert coadd.coadd_exp.image.array.shape == coadd_dims
-    assert coadd.image.shape == coadd_dims
-    assert coadd.noise.shape == coadd_dims
-    assert coadd.psf.image.shape == psf_dims
-
-    for band in coadds.bands:
-        bcoadd = coadds.get_coadd(band=band)
-        assert bcoadd.coadd_exp.image.array.shape == coadd_dims
-        assert bcoadd.image.shape == coadd_dims
-        assert bcoadd.noise.shape == coadd_dims
-        assert bcoadd.psf.image.shape == psf_dims
-
-    # not coadding individual bands
-    coadds = MultiBandCoaddsDM(
-        data=data['band_data'],
-        coadd_wcs=data['coadd_wcs'],
-        coadd_bbox=data['coadd_bbox'],
-        psf_dims=psf_dims,
-        byband=False,
-    )
-
-    for band in coadds.bands:
-        assert band not in coadds.coadds
+        assert coadd.image.shape == coadd_dims
+        assert coadd.noise.shape == coadd_dims
+        assert coadd.psf.image.shape == psf_dims
 
 
 @pytest.mark.skipif(
@@ -119,38 +95,19 @@ def test_coadd_sim_pspsf_smoke():
 
     psf_dims = data['psf_dims']
 
-    # coadding individual bands as well as over bands
-    coadds = MultiBandCoaddsDM(
-        data=data['band_data'],
-        coadd_wcs=data['coadd_wcs'],
-        coadd_bbox=data['coadd_bbox'],
-        psf_dims=psf_dims,
-    )
+    for band, exps in data['band_data'].items():
+        coadd = make_online_coadd_obs(
+            exps=exps,
+            coadd_wcs=data['coadd_wcs'],
+            coadd_bbox=data['coadd_bbox'],
+            psf_dims=psf_dims,
+            rng=rng,
+            remove_poisson=False,
+        )
 
-    coadd = coadds.get_coadd()
-    assert coadd.coadd_exp.image.array.shape == coadd_dims
-    assert coadd.image.shape == coadd_dims
-    assert coadd.noise.shape == coadd_dims
-    assert coadd.psf.image.shape == psf_dims
-
-    for band in coadds.bands:
-        bcoadd = coadds.get_coadd(band=band)
-        assert bcoadd.coadd_exp.image.array.shape == coadd_dims
-        assert bcoadd.image.shape == coadd_dims
-        assert bcoadd.noise.shape == coadd_dims
-        assert bcoadd.psf.image.shape == psf_dims
-
-    # not coadding individual bands
-    coadds = MultiBandCoaddsDM(
-        data=data['band_data'],
-        coadd_wcs=data['coadd_wcs'],
-        coadd_bbox=data['coadd_bbox'],
-        psf_dims=psf_dims,
-        byband=False,
-    )
-
-    for band in coadds.bands:
-        assert band not in coadds.coadds
+        assert coadd.image.shape == coadd_dims
+        assert coadd.noise.shape == coadd_dims
+        assert coadd.psf.image.shape == psf_dims
 
 
 def test_coadd_sim_noise():
@@ -158,31 +115,34 @@ def test_coadd_sim_noise():
     ensure the psf and noise var are set close, so
     that the relative weight is right
     """
-    rng = np.random.RandomState(32)
-    data = _make_sim(rng=rng, psf_type='gauss')
+    rng = np.random.RandomState(325)
+
+    # turn off dither/rotate for testing noise
+    data = _make_sim(
+        rng=rng, psf_type='gauss',
+        dither=False, rotate=False,
+    )
 
     psf_dims = data['psf_dims']
 
-    # coadding individual bands as well as over bands
-    coadds = MultiBandCoaddsDM(
-        data=data['band_data'],
-        coadd_wcs=data['coadd_wcs'],
-        coadd_bbox=data['coadd_bbox'],
-        psf_dims=psf_dims,
-    )
+    for band, exps in data['band_data'].items():
+        coadd = make_online_coadd_obs(
+            exps=exps,
+            coadd_wcs=data['coadd_wcs'],
+            coadd_bbox=data['coadd_bbox'],
+            psf_dims=psf_dims,
+            rng=rng,
+            remove_poisson=False,
+        )
 
-    for exp, pexp, nexp in zip(coadds.exps,
-                               coadds.psf_exps,
-                               coadds.noise_exps):
-
-        emed = np.median(exp.variance.array)
-        pmed = np.median(pexp.variance.array)
-        nmed = np.median(nexp.variance.array)
+        emed = np.min(coadd.coadd_exp.variance.array)
+        pmed = np.min(coadd.coadd_psf_exp.variance.array)
+        nmed = np.min(coadd.coadd_noise_exp.variance.array)
 
         assert abs(pmed/emed-1) < 1.0e-3
         assert abs(nmed/emed-1) < 1.0e-3
 
-        bmask = nexp.mask.array
+        bmask = coadd.coadd_noise_exp.mask.array
         w = np.where(bmask == 0)
-        nvar = nexp.image.array[w].var()
+        nvar = coadd.coadd_noise_exp.image.array[w].var()
         assert abs(nvar / emed - 1) < 0.02
--- descwl_coadd/tests/test_online_coadds.py
+++ descwl_coadd/tests/test_online_coadds.py
@@ -1,43 +1,90 @@
+import os
+import pytest
 import numpy as np
 
-from descwl_shear_sims.sim import make_dmsim
-from descwl_shear_sims.sim.galaxy_catalogs import FixedGalaxyCatalog
-from descwl_shear_sims.sim.psfs import make_psf
+from descwl_shear_sims.sim import make_sim, get_se_dim
+from descwl_shear_sims.psfs import make_fixed_psf, make_ps_psf
+from descwl_shear_sims.stars import StarCatalog
 
 from ..online_coadds import make_online_coadd_obs
+from descwl_shear_sims.galaxies import make_galaxy_catalog
 
 
-def test_online_coadds_smoke():
-    rng = np.random.RandomState(313)
-    coadd_dim = 100
-    psf_dim = 51
-    buff = 10
-    cat = FixedGalaxyCatalog(
-        rng=rng, coadd_dim=coadd_dim, buff=buff, layout='grid',
-        mag=22, hlr=0.5,
-    )
-    g1 = 0.02
-    g2 = 0.00
-    psf = make_psf(psf_type='gauss')
-    bands = ['r', 'i', 'z']
-    epochs_per_band = 3
-    sim_data = make_dmsim(
+def _make_sim(
+    rng, psf_type,
+    epochs_per_band=3,
+    stars=False,
+    dither=True,
+    rotate=True,
+    bands=['i', 'z'],
+    coadd_dim=101,
+    psf_dim=51,
+):
+    buff = 5
+
+    galaxy_catalog = make_galaxy_catalog(
         rng=rng,
-        galaxy_catalog=cat,
+        gal_type="exp",
         coadd_dim=coadd_dim,
-        g1=g1,
-        g2=g2,
-        psf=psf,
-        bands=bands,
-        epochs_per_band=epochs_per_band,
+        buff=buff,
+        layout="grid",
+        gal_config={'mag': 22},
     )
 
+    if psf_type == "ps":
+        se_dim = get_se_dim(coadd_dim=coadd_dim)
+        psf = make_ps_psf(rng=rng, dim=se_dim)
+    else:
+        psf = make_fixed_psf(psf_type=psf_type)
+
+    if stars:
+        star_catalog = StarCatalog(
+            rng=rng,
+            coadd_dim=coadd_dim,
+            buff=buff,
+            density=100,
+        )
+    else:
+        star_catalog = None
+
+    return make_sim(
+        rng=rng,
+        galaxy_catalog=galaxy_catalog,
+        star_catalog=star_catalog,
+        coadd_dim=coadd_dim,
+        psf_dim=psf_dim,
+        epochs_per_band=epochs_per_band,
+        g1=0.02,
+        g2=0.00,
+        bands=bands,
+        psf=psf,
+        dither=dither,
+        rotate=dither,
+    )
+
+
+@pytest.mark.parametrize('dither', [False, True])
+@pytest.mark.parametrize('rotate', [False, True])
+def test_online_coadds_smoke(dither, rotate):
+    rng = np.random.RandomState(55)
+
+    coadd_dim = 101
+    psf_dim = 51
+
+    bands = ['r', 'i', 'z']
+    sim_data = _make_sim(
+        rng=rng, psf_type='gauss', bands=bands,
+        coadd_dim=coadd_dim, psf_dim=psf_dim,
+        dither=dither, rotate=rotate,
+    )
+
+    # coadd each band separately
     bdata = sim_data['band_data']
     for band in bands:
         assert band in bdata
-        exps = [data['exp'] for data in bdata[band]]
+        exps = bdata[band]
 
-        coadd_obs = make_online_coadd_obs(
+        coadd = make_online_coadd_obs(
             exps=exps,
             coadd_wcs=sim_data['coadd_wcs'],
             coadd_bbox=sim_data['coadd_bbox'],
@@ -48,5 +95,107 @@ def test_online_coadds_smoke():
 
         coadd_dims = (coadd_dim, )*2
         psf_dims = (psf_dim, )*2
-        assert coadd_obs.image.shape == coadd_dims
-        assert coadd_obs.psf.image.shape == psf_dims
+        assert coadd.image.shape == coadd_dims
+        assert coadd.psf.image.shape == psf_dims
+
+
+def test_coadd_sim_psgauss_smoke():
+    psf_type = 'gauss'
+    rng = np.random.RandomState(8312)
+    data = _make_sim(rng=rng, psf_type=psf_type)
+
+    extent = data['coadd_bbox'].getDimensions()
+    coadd_dims = (extent.getX(), extent.getY())
+    assert data['coadd_dims'] == coadd_dims
+
+    psf_dims = data['psf_dims']
+
+    for band, exps in data['band_data'].items():
+        coadd = make_online_coadd_obs(
+            exps=exps,
+            coadd_wcs=data['coadd_wcs'],
+            coadd_bbox=data['coadd_bbox'],
+            psf_dims=psf_dims,
+            rng=rng,
+            remove_poisson=False,
+        )
+
+        assert coadd.image.shape == coadd_dims
+        assert coadd.noise.shape == coadd_dims
+        assert coadd.psf.image.shape == psf_dims
+
+
+@pytest.mark.skipif(
+    "CATSIM_DIR" not in os.environ,
+    reason='simulation input data is not present'
+)
+def test_coadd_sim_pspsf_smoke():
+    psf_type = 'ps'
+    rng = np.random.RandomState(8312)
+    data = _make_sim(rng=rng, psf_type=psf_type, stars=True)
+
+    extent = data['coadd_bbox'].getDimensions()
+    coadd_dims = (extent.getX(), extent.getY())
+    assert data['coadd_dims'] == coadd_dims
+
+    psf_dims = data['psf_dims']
+
+    for band, exps in data['band_data'].items():
+        coadd = make_online_coadd_obs(
+            exps=exps,
+            coadd_wcs=data['coadd_wcs'],
+            coadd_bbox=data['coadd_bbox'],
+            psf_dims=psf_dims,
+            rng=rng,
+            remove_poisson=False,
+        )
+
+        assert coadd.image.shape == coadd_dims
+        assert coadd.noise.shape == coadd_dims
+        assert coadd.psf.image.shape == psf_dims
+
+
+@pytest.mark.parametrize('dither', [False, True])
+@pytest.mark.parametrize('rotate', [False, True])
+def test_online_coadds_noise(dither, rotate):
+    rng = np.random.RandomState(55)
+
+    # noise test currently only works with odd due to pixel offsets
+    # used for psf coadding.  Same goes for dithering
+    coadd_dim = 101
+    psf_dim = 51
+
+    bands = ['r', 'i', 'z']
+    sim_data = _make_sim(
+        rng=rng, psf_type='gauss', bands=bands,
+        coadd_dim=coadd_dim, psf_dim=psf_dim,
+        dither=dither, rotate=rotate,
+    )
+
+    # coadd each band separately
+    bdata = sim_data['band_data']
+    for band in bands:
+        assert band in bdata
+        exps = bdata[band]
+
+        coadd = make_online_coadd_obs(
+            exps=exps,
+            coadd_wcs=sim_data['coadd_wcs'],
+            coadd_bbox=sim_data['coadd_bbox'],
+            psf_dims=sim_data['psf_dims'],
+            rng=rng,
+            remove_poisson=False,  # no object poisson noise in sims
+        )
+
+        coadd_dims = (coadd_dim, )*2
+        psf_dims = (psf_dim, )*2
+        assert coadd.image.shape == coadd_dims
+        assert coadd.psf.image.shape == psf_dims
+
+        if not dither:
+            emed = np.median(coadd.coadd_exp.variance.array)
+            pmed = np.median(coadd.coadd_psf_exp.variance.array)
+            nmed = np.median(coadd.coadd_noise_exp.variance.array)
+
+            assert abs(pmed/emed-1) < 1.0e-3
+            assert abs(nmed/emed-1) < 1.0e-3
