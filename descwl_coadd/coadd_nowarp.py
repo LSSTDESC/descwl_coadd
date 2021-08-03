1435954d726cad4d73ed062e3c3770e8f2d7fa65
Erin Sheldon <erin.sheldon@gmail.com>
2021-08-03 11:14:59 -0400

    simpler warp, one exp only

--- descwl_coadd/coadd_nowarp.py
+++ descwl_coadd/coadd_nowarp.py
@@ -1,28 +1,17 @@
-"""
-Coadding without warping
-"""
-import numpy as np
 from .coadd import (
     DEFAULT_LOGLEVEL,
     CoaddObs,
     make_logger,
     check_psf_dims,
     get_coadd_center,
-    get_coadd_psf_bbox,
-    make_coadd_exposure,
-    make_stacker,
     get_exp_and_noise,
     get_psf_exp,
-    get_exp_weight,
-    verify_coadd_edges,
     flag_bright_as_sat_in_coadd,
-    extract_coadd_psf,
 )
-from esutil.pbar import PBar
 
 
 def make_coadd_obs_nowarp(
-    exps, psf_dims, rng, remove_poisson,
+    exp, psf_dims, rng, remove_poisson,
     loglevel=DEFAULT_LOGLEVEL,
 ):
     """
@@ -32,8 +21,8 @@ def make_coadd_obs_nowarp(
 
     Parameters
     ----------
-    exps: list
-        Either a list of exposures or a list of DeferredDatasetHandle
+    exp: Exposure
+        Exposure to adapt to coadd observation form
     psf_dims: tuple
         The dimensions of the psf
     rng: np.random.RandomState
@@ -50,7 +39,7 @@ def make_coadd_obs_nowarp(
     """
 
     coadd_data = make_coadd_nowarp(
-        exps=exps,
+        exp=exp,
         psf_dims=psf_dims,
         rng=rng, remove_poisson=remove_poisson,
         loglevel=loglevel,
@@ -69,7 +58,7 @@ def make_coadd_obs_nowarp(
 
 
 def make_coadd_nowarp(
-    exps, psf_dims, rng, remove_poisson,
+    exp, psf_dims, rng, remove_poisson,
     loglevel=DEFAULT_LOGLEVEL,
 ):
     """
@@ -79,7 +68,7 @@ def make_coadd_nowarp(
 
     Parameters
     ----------
-    exps: list
+    exp: Exposure
         Either a list of exposures or a list of DeferredDatasetHandle
     psf_dims: tuple
         The dimensions of the psf
@@ -107,123 +96,31 @@ def make_coadd_nowarp(
     """
 
     logger = make_logger('coadd', loglevel)
+    logger.info('making coadd obs')
+
     check_psf_dims(psf_dims)
 
-    example_exp = exps[0]
-    coadd_wcs = example_exp.wcs
-    coadd_bbox = example_exp.getBBox()
-
-    # sky center of this coadd within bbox
-    coadd_cen, coadd_cen_skypos = get_coadd_center(
-        coadd_wcs=coadd_wcs, coadd_bbox=coadd_bbox,
+    _, noise_exp, var, mfrac_exp = get_exp_and_noise(
+        exp_or_ref=exp, rng=rng, remove_poisson=remove_poisson,
+    )
+    cen, cen_skypos = get_coadd_center(
+        coadd_wcs=exp.wcs, coadd_bbox=exp.getBBox(),
     )
 
-    coadd_psf_bbox = get_coadd_psf_bbox(
-        x=coadd_cen.x, y=coadd_cen.y, dim=psf_dims[0],
+    psf_exp = get_psf_exp(
+        exp=exp,
+        coadd_cen_skypos=cen_skypos,
+        var=var,
     )
-    coadd_psf_wcs = coadd_wcs
 
-    # separately stack data, noise, and psf
-    coadd_exp = make_coadd_exposure(coadd_bbox, coadd_wcs)
-    coadd_noise_exp = make_coadd_exposure(coadd_bbox, coadd_wcs)
-    coadd_psf_exp = make_coadd_exposure(coadd_psf_bbox, coadd_psf_wcs)
-    coadd_mfrac_exp = make_coadd_exposure(coadd_bbox, coadd_wcs)
-
-    coadd_dims = example_exp.image.array.shape
-    stacker = make_stacker(coadd_dims=coadd_dims)
-    noise_stacker = make_stacker(coadd_dims=coadd_dims)
-    psf_stacker = make_stacker(coadd_dims=psf_dims)
-    mfrac_stacker = make_stacker(coadd_dims=coadd_dims)
-
-    # will zip these with the exposures to add
-    stackers = [stacker, noise_stacker, psf_stacker, mfrac_stacker]
-    bboxes = [coadd_bbox, coadd_bbox, coadd_psf_bbox, coadd_bbox]
-
-    # PSF will generally have NO_DATA in warp as we tend to use the same psf
-    # stamp size for input and output psf and just zero out wherever there is
-    # no data
-
-    verify = [True, True, False, True]
-
-    nuse = 0
-    logger.info('adding exposures')
-
-    ormask = np.zeros(coadd_dims, dtype='i4')
-    ormasks = [ormask, None, None, None]
-
-    for exp_or_ref in PBar(exps):
-        exp, noise_exp, var, mfrac_exp = get_exp_and_noise(
-            exp_or_ref=exp_or_ref, rng=rng, remove_poisson=remove_poisson,
-        )
-        if exp is None:
-            continue
-
-        psf_exp = get_psf_exp(
-            exp=exp,
-            coadd_cen_skypos=coadd_cen_skypos,
-            var=var,
-        )
-
-        assert psf_exp.variance.array[0, 0] == noise_exp.variance.array[0, 0]
-
-        weight = get_exp_weight(exp)
-
-        # order must match stackers, bboxes
-        exps2add = [exp, noise_exp, psf_exp, mfrac_exp]
-
-        for _stacker, _exp, _bbox, _verify, _ormask in zip(
-            stackers, exps2add, bboxes, verify, ormasks
-        ):
-            add_nowarp(_stacker, _exp, _bbox, weight, _verify, _ormask)
-
-        nuse += 1
-
-    if nuse == 0:
-        return None
-
-    stacker.fill_stacked_masked_image(coadd_exp.maskedImage)
-    noise_stacker.fill_stacked_masked_image(coadd_noise_exp.maskedImage)
-    psf_stacker.fill_stacked_masked_image(coadd_psf_exp.maskedImage)
-    mfrac_stacker.fill_stacked_masked_image(coadd_mfrac_exp.maskedImage)
-
-    verify_coadd_edges(coadd_exp)
-    verify_coadd_edges(coadd_noise_exp)
-
-    flag_bright_as_sat_in_coadd(coadd_exp, ormask)
-    flag_bright_as_sat_in_coadd(coadd_noise_exp, ormask)
-
-    logger.info('making psf')
-    psf = extract_coadd_psf(coadd_psf_exp, logger)
-    coadd_exp.setPsf(psf)
-    coadd_noise_exp.setPsf(psf)
+    ormask = exp.mask.array.copy()
+    flag_bright_as_sat_in_coadd(exp, ormask)
+    flag_bright_as_sat_in_coadd(noise_exp, ormask)
 
     return dict(
-        coadd_exp=coadd_exp,
-        coadd_noise_exp=coadd_noise_exp,
-        coadd_psf_exp=coadd_psf_exp,
-        coadd_mfrac_exp=coadd_mfrac_exp,
+        coadd_exp=exp,
+        coadd_noise_exp=noise_exp,
+        coadd_psf_exp=psf_exp,
+        coadd_mfrac_exp=mfrac_exp,
         ormask=ormask,
     )
-
-
-def add_nowarp(stacker, exp, weight, ormask):
-    """
-    Add the exposure
-
-    Parameters
-    ----------
-    stacker: AccumulatorMeanStack
-        A stacker, type
-        lsst.pipe.tasks.accumulatorMeanStack.AccumulatorMeanStack
-    exp: afw_image.ExposureF
-        The exposure to add
-    weight: float
-        Weight for this image in the stack
-    ormask: array
-        This will be or'ed with the warped mask
-    """
-
-    if ormask is not None:
-        ormask |= exp.mask.array
-
-    stacker.add_masked_image(exp, weight=weight)
