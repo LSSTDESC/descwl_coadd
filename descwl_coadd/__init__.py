0f1ecbcbc57b4ba8d45c69ed5b5de092ffc4f991
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2023-08-25 08:21:46 -0700

    Expose DEFAULT_INTERP and MAX_MASKFRAC
    These ones should be configurable from the PipelineTask and not set
    within this library. The values here can act as a sensible default
    values.

--- descwl_coadd/__init__.py
+++ descwl_coadd/__init__.py
@@ -4,6 +4,7 @@ from .version import __version__
 
 from . import coadd
 from .coadd import make_coadd_obs, make_coadd, make_warps
+from .coadd import DEFAULT_INTERP, MAX_MASKFRAC
 from . import coadd_nowarp
 from .coadd_nowarp import make_coadd_obs_nowarp, make_coadd_nowarp
 from . import coadd_obs
