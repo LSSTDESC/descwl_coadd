ecf822ce61f00ad95a2875b6643543526bf5b9d1
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2023-12-01 09:12:50 -0800

    Expose more methods and attributes outside

--- descwl_coadd/__init__.py
+++ descwl_coadd/__init__.py
@@ -3,8 +3,16 @@
 from .version import __version__
 
 from . import coadd
-from .coadd import make_coadd_obs, make_coadd, warp_exposures, warp_psf
-from .coadd import DEFAULT_INTERP, MAX_MASKFRAC
+from .coadd import (make_coadd_obs,
+                    make_coadd,
+                    warp_exposures,
+                    warp_psf,
+                    make_stacker,
+                    get_bad_mask,
+                    get_info_struct,
+                    get_median_var,
+)
+from .coadd import DEFAULT_INTERP, MAX_MASKFRAC, FLAGS2INTERP
 from . import coadd_nowarp
 from .coadd_nowarp import make_coadd_obs_nowarp, make_coadd_nowarp
 from . import coadd_obs
